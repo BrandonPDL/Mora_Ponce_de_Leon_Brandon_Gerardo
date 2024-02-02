e1112a10aab47249f37c18ca200a774410ec00a5
Brandon Mora <94004688+sp-javier@users.noreply.github.com>
2024-02-02 17:56:27 -0600

    mio

--- FrontExamen/wwwroot/js/site.js
+++ FrontExamen/wwwroot/js/site.js
@@ -1,19 +1,1 @@
 ﻿
-    $(document).ready(function () {
-        $("#agregarExamenForm").submit(function (event) {
-            event.preventDefault();
-
-            var examenData = {
-                idExamen: $("#idExamen").val(),
-                nombre: $("#nombre").val(),
-                descripcion: $("#descripcion").val()
-            };
-
-            $.post('@Url.Action("Agregar", "ExamenController")', examenData, function (data) {
-                // Manejar la respuesta. Por ejemplo, cerrar el modal y actualizar la tabla
-                $('#agregarExamenModal').modal('hide');
-                // Puedes agregar código aquí para actualizar la tabla o mostrar un mensaje
-            });
-        });
-    });
-
